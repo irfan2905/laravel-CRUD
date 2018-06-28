651872441de9cab5c21e3ced97b375ef8838e54b
irfan2905 <irfan.naufal16@gmail.com>
2018-06-28 20:03:46 +0700

    Add existing file

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -68,24 +68,14 @@
             </form>
         </div>
 
-        <div class="social-auth-links text-center">
-            <div class="container">
-                <div class="row">
-                    <div class="col-md-12">
-                        <a href="{{url('/redirect')}}" class="btn btn-primary">
-                            <i class="fa fa-facebook"></i> Login With Facebook
-                        </a>              
-                    </div>
-                </div>
-            </div>
-            <p>- OR -</p>
-            <a href="#" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
-                Facebook</a>
-            <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
-                Google+</a>
-        </div>
+        <a href="{{url('/redirect')}}" class="btn btn-block btn-social btn-facebook btn-flat"><i class="fa fa-facebook"></i> Sign in using
+            Facebook</a>
+        <p>- OR -</p>
+        <a href="#" class="btn btn-block btn-social btn-google btn-flat"><i class="fa fa-google-plus"></i> Sign in using
+            Google+</a>
     </div>
-    <!-- /.login-box-body -->
+</div>
+<!-- /.login-box-body -->
 </div>
 <!-- /.login-box -->
 @endsection
