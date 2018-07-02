6a0c41f915202b71d2cb42421e67d6df85b3393d
irfan2905 <irfan.naufal16@gmail.com>
2018-07-03 02:48:14 +0700

    product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -177,6 +177,7 @@ class ProductController extends Controller
         $oldcart = Session::get('cart');
         $cart = new Cart($oldcart);
         $totalPrice = $cart->totalPrice;
+        var_dump($totalPrice);exit;
         return redirect('paywith');
     }
 }
