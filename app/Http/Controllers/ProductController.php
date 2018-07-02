57b42fc3ea7104f275ee9669b993f164a0fbe2b5
irfan2905 <irfan.naufal16@gmail.com>
2018-07-03 02:52:18 +0700

    product

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -177,7 +177,6 @@ class ProductController extends Controller
         $oldcart = Session::get('cart');
         $cart = new Cart($oldcart);
         $totalPrice = $cart->totalPrice;
-        var_dump($totalPrice);exit;
-        return redirect('paywith');
+        return view('paypal.paywithpaypal');
     }
 }
