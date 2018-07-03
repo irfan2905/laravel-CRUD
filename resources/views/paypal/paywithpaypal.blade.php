662265c0cec30bc054fe96170812414796e1b83b
irfan2905 <irfan.naufal16@gmail.com>
2018-07-03 18:34:01 +0700

    product

--- resources/views/paypal/paywithpaypal.blade.php
+++ resources/views/paypal/paywithpaypal.blade.php
@@ -26,8 +26,8 @@
                 <h2 class="w3-text-blue">Payment Form</h2>
                 <p>Demo PayPal form - Integrating paypal in laravel</p>
                 <p>      
-                    <label class="w3-text-blue"><b>Enter Amount</b></label>
-                    <input class="w3-input w3-border" name="totalPrice" type="number" value="{{ $totalPrice }}"></p>      
+                    <label class="w3-text-blue"><b>Enter Amount  $ </b></label>
+                    <input class="w3-input w3-border" name="amount" type="number" value="{{ $totalPrice }}"></p>      
                 <button class="w3-btn w3-blue">Pay with PayPal</button></p>
             </form>
         </div>
